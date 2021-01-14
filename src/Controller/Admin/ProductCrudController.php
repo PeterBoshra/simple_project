de237d76d9d98737464338f5ae5a018b93775baf
Peter Boshra <peter.boshra4@gmail.com>
2021-01-15 00:03:44 +0200

    Adding Symfony Notifier and send email after adding product

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -3,14 +3,26 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\NotifierInterface;
+use Symfony\Component\Notifier\Recipient\Recipient;
 
 class ProductCrudController extends AbstractCrudController
 {
+
+    private $notifier;
+
+    public function __construct(NotifierInterface $notifier)
+    {
+        $this->notifier = $notifier;
+    }
+
     public static function getEntityFqcn(): string
     {
         return Product::class;
@@ -25,6 +37,25 @@ class ProductCrudController extends AbstractCrudController
             IntegerField::new('price'),
             AssociationField::new('currency'),
         ];
+    }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $product): void
+    {
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        $notification = (new Notification('New Product', ['email']))
+            ->content('New Product Added'. $product->getName());
+
+        // The receiver of the Notification
+        $recipient = new Recipient(
+            'peter.boshra4@gmail.com',
+            '+201220896536'
+        );
+
+        // Send the notification to the recipient
+        $this->notifier->send($notification, $recipient);
+
     }
 
 }
